3392b5f3c9ce966ef38f59ca003c8037972e0598
Tanghaiyan <584536319@qq.com>
2016-11-26 17:05:48 +0800

    响应式修改

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+
+(function(){
+
+	// $('.top-nav li').click(function(e){
+	// 	console.log(1);
+		$('.top-nav').removeClass('.active');
+		$(this).addClass('active');
+	
+})();
+
